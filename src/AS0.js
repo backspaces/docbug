d4582e5938bd7db8576400fd7660a0a8d927c332
backspaces <owen@backspaces.net>
2021-07-28 12:41:05 -0600

    first commit

---
+++ src/AS0.js
@@ -0,0 +1,42 @@
+// This is the importer/exporter of our modules w/ dependencies.
+// I.e. 2D/3D Models and 2D/3D views
+// It is only used by Rollup for bundling.
+import { Object3D } from '../vendor/Object3D.js'
+
+// Models:
+export { default as AgentArray } from './AgentArray.js'
+export { default as AgentSet } from './AgentSet.js'
+export { default as AgentList } from './AgentList.js'
+export { default as DataSet } from './DataSet.js'
+export { default as Link } from './Link.js'
+export { default as Links } from './Links.js'
+// export { gis }
+export * as gis from './gis.js'
+export { default as Model } from './Model.js'
+export { default as Model2D } from './Model2D.js'
+export { Object3D }
+export { default as Patch } from './Patch.js'
+export { default as Patches } from './Patches.js'
+export { default as RGBDataSet } from './RGBDataSet.js'
+export * as RGBADataSet from './RGBADataSet.js'
+export * as TileDataSet from './TileDataSet.js'
+export { default as Turtle } from './Turtle.js'
+export { default as Turtles } from './Turtles.js'
+export { default as World } from './World.js'
+// export { util }
+export * as util from './utils.js'
+
+// Views
+export { default as Color } from './Color.js'
+export { default as ColorMap } from './ColorMap.js'
+export { default as Shapes } from './Shapes.js'
+export { default as SpriteSheet } from './SpriteSheet.js'
+export { default as TwoView } from './TwoView.js'
+export { default as TwoDraw } from './TwoDraw.js'
+// export { default as ThreeView } from './ThreeView.js'
+// export { default as ThreeMeshes } from './ThreeMeshes.js'
+// export { default as ThreeDraw } from './ThreeDraw.js'
+
+// Controls
+export { default as Animator } from './Animator.js'
+export { default as Mouse } from './Mouse.js'
